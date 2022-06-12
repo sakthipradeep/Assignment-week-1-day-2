9cc10f3e6b2eae528b4a0937c325073d279a7060
sakthipradeep <105548072+sakthipradeep@users.noreply.github.com>
2022-06-12 16:30:13 +0530

    SUB CLASS OF CAR

---
+++ MyCar.java
@@ -0,0 +1,15 @@
+package week1.day2.Assignment;
+import  week1.day2.Assignment.TwoWheeler;
+public class MyCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car SWIFT=new Car();
+		SWIFT.startCar();
+		SWIFT.applyGear();
+		SWIFT.applyAcclerate();
+		SWIFT.switchONac();
+		SWIFT.applyBreak();
+	}
+
+}
